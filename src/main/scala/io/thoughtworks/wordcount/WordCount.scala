1bbef322e2e65daf11a1774fdbf17bd6e4e3bac2
yaxin.wang <yaxin.wang@rea-group.com>
2020-05-28 15:04:55 +0800

    Complete..

--- src/main/scala/io/thoughtworks/wordcount/WordCount.scala
+++ src/main/scala/io/thoughtworks/wordcount/WordCount.scala
@@ -3,7 +3,35 @@ package io.thoughtworks.wordcount
 case class WordFrequency(word: String, counts: Int)
 
 class WordCount {
-  def listWords(text: String): List[WordFrequency]      = ???
-  def countWords(words: List[String]): Map[String, Int] = ???
-  def extractWords(text: String): List[String]          = ???
+  def listWords(text: String): List[WordFrequency] =
+    (extractWords _)
+      .andThen(countWords)
+      .apply(text)
+      .map {
+        case (word, count) => WordFrequency(word, count)
+      }
+      .toList
+      .sortBy(_.counts)
+
+//  def listWords(text: String): List[WordFrequency] =
+//    countWords(extractWords(text)).toList
+//      .map {
+//        case (word, count) =>
+//          WordFrequency(word, count)
+//      }
+//      .sortBy(_.counts)
+
+  def countWords(words: List[String]): Map[String, Int] =
+    words.groupBy(identity).map { case (word, value) => (word, value.size) }
+
+  def extractWords(text: String): List[String] =
+    text
+      .replace(",", "")
+      .replace("-", " ")
+      .replace("\"", "")
+      .replace(".", "")
+      .toLowerCase
+      .split("\\s+")
+      .filter(_.nonEmpty)
+      .toList
 }
